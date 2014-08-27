614f524eed994f09c054c47935648012566d0749
David-NF <neurofibromatose@live.nl>
2014-08-27 10:48:07 +0200

    eerste vertaling

--- nl/modules/portal_latest_members_module.php
+++ nl/modules/portal_latest_members_module.php
@@ -1,6 +1,7 @@
 <?php
 /**
 *
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 * @package Board3 Portal v2.1 - Latest Members
 * @copyright (c) 2013 Board3 Group ( www.board3.de )
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
@@ -32,11 +33,11 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 $lang = array_merge($lang, array(
-	'LATEST_MEMBERS'	=> 'Newest members',
+	'LATEST_MEMBERS'	=> 'Nieuwste gebruikers',
 
 	// ACP
-	'ACP_PORTAL_MEMBERS_SETTINGS'			=> 'Newest members settings',
-	'ACP_PORTAL_MEMBERS_SETTINGS_EXP'	=> 'This is where you customize the newest members block.',
-	'PORTAL_MAX_LAST_MEMBER'				=> 'Maximum number of newest members shown',
-	'PORTAL_MAX_LAST_MEMBER_EXP'		=> '0 means infinite',
+	'ACP_PORTAL_MEMBERS_SETTINGS'			=> 'Nieuwste gebruikers instellingen',
+	'ACP_PORTAL_MEMBERS_SETTINGS_EXP'	=> 'Hier kan je het nieuwste gebruikers blok aanpassen.',
+	'PORTAL_MAX_LAST_MEMBER'				=> 'Aantal getoonde nieuwe gebruikers',
+	'PORTAL_MAX_LAST_MEMBER_EXP'		=> 'Het maximum aantal nieuwe gebruikers dat wordt weergegeven, 0 is onbeperkt',
 ));
