5c390623a4cc53356692435160580bc3a523c005
David-NF <neurofibromatose@live.nl>
2014-09-03 14:45:43 +0200

    eerste vertaling

--- nl/modules/portal_poll_module.php
+++ nl/modules/portal_poll_module.php
@@ -1,6 +1,7 @@
 <?php
 /**
 *
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 * @package Board3 Portal v2.1 - Poll
 * @copyright (c) 2013 Board3 Group ( www.board3.de )
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
@@ -33,21 +34,21 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 $lang = array_merge($lang, array(
 	'PORTAL_POLL'			=> 'Poll',
-	'LATEST_POLLS'			=> 'Latest Polls',
-	'NO_OPTIONS'			=> 'This poll has no available options.',
-	'NO_POLL'				=> 'No polls available',
-	'RETURN_PORTAL'			=> '%sReturn to the portal%s',
+	'LATEST_POLLS'			=> 'Laatste Polls',
+	'NO_OPTIONS'			=> 'Deze poll heeft geen beschikbare opties.',
+	'NO_POLL'				=> 'Geen polls beschrikbaar',
+	'RETURN_PORTAL'			=> '%sGa terug naar het portaal%s',
 
 	// ACP
-	'ACP_PORTAL_POLLS_SETTINGS'			=> 'Poll settings',
-	'ACP_PORTAL_POLLS_SETTINGS_EXP'	=> 'This is where you customize the poll block.',
+	'ACP_PORTAL_POLLS_SETTINGS'			=> 'Poll instellingen',
+	'ACP_PORTAL_POLLS_SETTINGS_EXP'	=> 'Hier kan je het poll blok aanpassen.',
 	'PORTAL_POLL_TOPIC_ID'				=> 'Poll forum(s)',
-	'PORTAL_POLL_TOPIC_ID_EXP'		=> 'The forum(s) from which the polls should be displayed. If "Exclude forums" is set to "Yes", select the forums you want to exclude.<br />If "Exclude forums" is set to "No" select the forums you want to see.<br />Select/Deselect multiple forums by holding <samp>CTRL</samp> and clicking.',
-	'PORTAL_POLL_EXCLUDE_ID'			=> 'Exclude Forums',
-	'PORTAL_POLL_EXCLUDE_ID_EXP'	=> 'Select "Yes" if you want to exlude the selected forums from the polls block, and "No" if you only want to see the polls from the selected forums in the polls block.',
-	'PORTAL_POLL_LIMIT'					=> 'Poll display limit',
-	'PORTAL_POLL_LIMIT_EXP'			=> 'The number of polls you would like to display on the portal page.',
-	'PORTAL_POLL_ALLOW_VOTE'			=> 'Allow voting',
-	'PORTAL_POLL_ALLOW_VOTE_EXP'	=> 'Allow users with the required permissions to vote from the portal page.',
-	'PORTAL_POLL_HIDE'					=> 'Hide expired polls?',
+	'PORTAL_POLL_TOPIC_ID_EXP'		=> 'De forumonderdelen waarvan je de polls wilt weergeven. Als "Forumonderdelen uitsluiten" is ingesteld op "Ja", selecteer dan de forumonderdelen die je wilt uitsluiten.<br />Als "Forumonderdelen uitsluiten" is ingesteld op "Nee" selecteer dan de forumonderdelen die je wilt weergeven.<br />Selecteer/Deselecteer meerdere forumonderdelen doormiddel van <samp>CTRL</samp> en door te klikken.',
+	'PORTAL_POLL_EXCLUDE_ID'			=> 'Forumonderdelen uitsluiten',
+	'PORTAL_POLL_EXCLUDE_ID_EXP'	=> 'Selecteer "Ja" als je de geselecteerde forumonderdelen wilt uitsluiten in het poll blok, en "Nee" als je alleen de geselecteerde forumonderdelen wilt weergeven in het poll blok.',
+	'PORTAL_POLL_LIMIT'					=> 'Aantal polls weergeven',
+	'PORTAL_POLL_LIMIT_EXP'			=> 'Het aantal polls dat je wilt weergeven op de portaalpagina.',
+	'PORTAL_POLL_ALLOW_VOTE'			=> 'Sta stemmen toe',
+	'PORTAL_POLL_ALLOW_VOTE_EXP'	=> 'Sta gebruikers met de vereiste permissies toe om te stemmen vanaf de portaalpagina.',
+	'PORTAL_POLL_HIDE'					=> 'Verberg verlopen polls?',
 ));
