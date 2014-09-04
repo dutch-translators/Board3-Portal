9d2577a825b09d552c15df19f47e0637c268f5de
David-NF <neurofibromatose@live.nl>
2014-09-04 14:48:19 +0200

    eerste vertaling

--- nl/modules/portal_stylechanger_module.php
+++ nl/modules/portal_stylechanger_module.php
@@ -1,6 +1,7 @@
 <?php
 /**
 *
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 * @package Board3 Portal v2.1 - Stylechanger
 * @copyright (c) 2013 Board3 Group ( www.board3.de )
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
@@ -32,6 +33,6 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 $lang = array_merge($lang, array(
-	'BOARD_STYLE'			=> 'Board style',
-	'STYLE_CHOOSE'			=> 'Select a style',
+	'BOARD_STYLE'			=> 'Forumstijl',
+	'STYLE_CHOOSE'			=> 'Selecteer een stijl',
 ));
