d9de7083ad6a04a4dfa0fef88975d5946097d05c
David <neurofibromatose@live.nl>
2014-09-08 16:48:42 +0200

    credit lijn toegevoegd

--- nl/modules/portal_whois_online_module.php
+++ nl/modules/portal_whois_online_module.php
@@ -1,6 +1,7 @@
 <?php
 /**
 *
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 * @package Board3 Portal v2.1 - Who is online
 * @copyright (c) 2013 Board3 Group ( www.board3.de )
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
