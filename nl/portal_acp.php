2ab7abd3ab5339ac98e7d8165a39d0b9c02b3859
David <neurofibromatose@live.nl>
2014-11-26 19:45:19 +0100

    2.1.0-RC1

--- nl/portal_acp.php
+++ nl/portal_acp.php
@@ -74,6 +74,7 @@ $lang = array_merge($lang, array(
 	'MODULE_STATUS'				=> 'Module inschakelen',
 	'MODULE_ADD_ONCE'			=> 'Deze module kan maar één keer worden toegevoegd.',
 	'MODULE_IMAGE_ERROR'		=> 'Er is een fout opgetreden tijdens het controleren van de module afbeelding:',
+	'UNKNOWN_MODULE_METHOD'		=> 'De %1$s modules module methode kan niet worden opgelost.',
 
 	// general
 	'ACP_PORTAL_CONFIG_INFO'				=> 'Algemene instellingen',
